0ec4c3d32741ba04af0a27813c5352f8cf70805c
CohenK <cohenkang99@hotmail.com>
2025-04-06 19:50:48 -0700

    changed breakable wall to use Curve3D

--- Scripts/Objects/Interactables/breakable_wall.gd
+++ Scripts/Objects/Interactables/breakable_wall.gd
@@ -4,10 +4,8 @@ extends Interactable
 @export var container : Node3D
 
 func use(_player: Player) -> void:
-	for rock in container.get_children():
+	for node in container.get_children():
+		var path : Path3D = node.get_node("Path3D")
+		var path_follow = path.get_node("PathFollow3D")
 		var tween = create_tween()
-		tween.tween_property(rock, "global_position", global_position + Vector3.LEFT*0.5, 1)
-	
-	await get_tree().create_timer(1.5).timeout
-	for rock in container.get_children():
-		rock.get_node("AnimatableBody3D").collision_layer=0
+		tween.tween_property(path_follow, "progress", path.curve.get_baked_length(), 1)
