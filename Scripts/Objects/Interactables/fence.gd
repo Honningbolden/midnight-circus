626743252209eeee4fe10571ddfb3906d0ecc294
Jennifer Danielson <apdan150@gmail.com>
2025-04-14 14:00:27 -0700

    Added the fence.

--- Scripts/Objects/Interactables/fence.gd
+++ Scripts/Objects/Interactables/fence.gd
@@ -4,7 +4,10 @@ extends Interactable
 @export var container : Node3D
 
 func use(_player: Player) -> void:
-	#Task: make this work
 	for node in self.get_children():
 		var tween = create_tween()
 		tween.tween_property(self, "rotation", Vector3(0, 0, -PI/2), 1)
+		
+	# TASK: Make models for the fence
+	# TASK: Make sound effect
+	# TASK: Make the ferris wheel interract with the fence instead of the player
