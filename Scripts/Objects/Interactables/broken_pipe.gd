0334539a602326a10d45403ac150ef154ad49936
Angelo Cervana <152583644+angeloose@users.noreply.github.com>
2025-04-22 16:29:20 -0700

    Added duct tape and fix broken pipe puzzle
    Also added one line of code in player.gd that checked if raycasted object is null (for collectibles)

---
+++ Scripts/Objects/Interactables/broken_pipe.gd
@@ -0,0 +1,17 @@
+# broken_pipe.gd
+
+extends Interactable
+@onready var mesh = $MeshInstance3D
+@onready var material = mesh.get_active_material(0)
+
+func use(_player: Player) -> void:
+	if GameManager.collected_items.has("Duct Tape"):
+		GameManager.collected_items.erase("Duct Tape")
+		
+		# Duplicate to not effect other objs of same material
+		var unique_material = material.duplicate()
+		unique_material.albedo_color = Color(0, 1, 0)  # Green
+		mesh.set_surface_override_material(0, unique_material)
+		
+		# TASK: Add an animation for fixing pipe
+		# TASK: Add a sound effect for fixing pipe
