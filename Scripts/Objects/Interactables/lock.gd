c780dece248533646bca3c19af4c545de665c8e5
Angelo Cervana <152583644+angeloose@users.noreply.github.com>
2025-04-24 01:14:42 -0700

    Added (crappy) code display for safe and ringmaster puzzle code/test assets

--- Scripts/Objects/Interactables/lock.gd
+++ Scripts/Objects/Interactables/lock.gd
@@ -3,7 +3,9 @@ extends Node2D
 var pin_code = [1, 0, 1, 3, 2, 0, 0, 1]  #10/13/2001
 var fix = ["start", "start"]; var dir = "start"
 var rot = 0; var key = []; var code = []
+@onready var dial = $Dial
 @onready var turn_audio = $LockTurnAudio
+@onready var inputted_code = $Inputs
 
 @onready var input_delay: Timer = $Timer
 var input_allowed = false
@@ -17,7 +19,6 @@ func initialize():
 	input_delay.one_shot = true
 	set_process_input(true)
 	input_allowed = true
-	print('ready lock')
 
 func _input(_event):
 	if not input_allowed:
@@ -31,7 +32,7 @@ func _input(_event):
 		key.push_back(key[0])
 		key.pop_front()
 		rot -= deg_to_rad(36)
-		get_node("Dial").rotation = rot
+		dial.rotation = rot
 		turn_audio.play()
 		start_input_cooldown()
 
@@ -43,29 +44,36 @@ func _input(_event):
 		key.push_front(key[9])
 		key.pop_back()
 		rot += deg_to_rad(36)
-		get_node("Dial").rotation = rot
+		dial.rotation = rot
 		turn_audio.play()
 		start_input_cooldown()
+		
 	
 	# Exit puzzle UI 
 	if Input.is_key_pressed(KEY_Q):
-		var rot = 0
-		var key = []
-		var code = []
+		dial.rotation = 0
+		rot = 0
+		key = []
+		code = []
 		input_allowed = false
-		get_parent().puzzle_end()
+		get_parent().puzzle_end(false)
 	
-	# If player gets the correct code
+	# Pop if input size goes over the code size
 	if code.size() > pin_code.size():
 		code.pop_front()
-		print(code, code.size(), pin_code.size())
-		if code == pin_code:
-			print('Open')
-			get_parent().puzzle_end()
-			# remove and end script
-			queue_free()
-			#get_node("safe").set_frame(1)
-			#get_node("SamplePlayer").play("safe_open")
+			
+	# Display code to user
+	inputted_code.text = str(code)
+	
+	# If player gets the correct code
+	if code == pin_code:
+		inputted_code.add_theme_color_override("font_color", Color(0, 1, 0))
+		await get_tree().create_timer(1).timeout
+		get_parent().puzzle_end(true)
+		# remove and end script
+		queue_free()
+		#get_node("safe").set_frame(1)
+		#get_node("SamplePlayer").play("safe_open")
 	
 
 func start_input_cooldown():
