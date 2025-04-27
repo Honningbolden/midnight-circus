c2866d5b50ec4c135cfa890872d1fda7565ce8ee
CohenK <cohenkang99@hotmail.com>
2025-04-26 21:08:40 -0700

    implemented steam engine/valve animation

--- Scripts/Objects/Interactables/steam_engine.gd
+++ Scripts/Objects/Interactables/steam_engine.gd
@@ -1,9 +1,12 @@
 # steam_engine.gd
 
 extends Interactable
+@export var valve: MeshInstance3D
 
 
 func use(_player: Player) -> void:
 	if GameManager.collected_items.has("Steam Engine Valve"):
 		GameManager.collected_items.erase("Steam Engine Valve")
-		$AnimationPlayer.play("rotate valve")
+		valve.visible = true
+		var tween = create_tween()
+		tween.tween_property(valve, "rotation_degrees", Vector3(0,90,0), 1)
