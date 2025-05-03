ac6ad230a59b5450465bf1ff647f2ab4bbfed5dc
CohenK <kangcohen@gmail.com>
2025-05-02 23:00:52 -0700

    canon state updates

--- Scripts/Objects/Interactables/canon.gd
+++ Scripts/Objects/Interactables/canon.gd
@@ -2,5 +2,10 @@
 
 extends Interactable
 
+@export var camera: Camera3D
+
 func use(player: Player) -> void:
+	var canon_state = player.statemachine.get_node("Canon State")
+	canon_state.canon = self
+	player.reparent(self)
 	player.statemachine.change_state("Canon State")
--- Scripts/Player/State Machine/canon_state.gd
+++ Scripts/Player/State Machine/canon_state.gd
@@ -2,26 +2,31 @@
 
 extends StateMachineState
 
-
 @export var player : Player
-@export var aim_speed : float = 0.02
-@export var canon: Interactable
+@export var canon: Node3D
+@export var camera: Camera3D
+@export var angle: float
 
 var world : Node3D = null
-var loaded = false
+var loaded = true
+var aim_speed = 0.05
 
-func on_ready() -> void:
+
+func _ready() -> void:
 	world = player.get_parent()
 	
 func on_enter() -> void:
+	player.velocity = Vector3.ZERO
 	if loaded:
-		player.position = Vector3.ZERO
+		player.position = Vector3(0, -0.5,0)
+		player.forward_direction = 0.0
+		camera.set_camera()
 	else:
 		if GameManager.collected_items.has("Canonball"):
 			GameManager.collected_items.erase("Canonball")
 			loaded = true
 		else:
-			on_exit()
+			change_state("Exploration State")
 
 func on_process(_delta: float) -> void:
 	player.update_input()  # take the player's input
@@ -29,6 +34,9 @@ func on_process(_delta: float) -> void:
 func on_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		change_state("Exploration State")
+	elif event is InputEventMouseMotion:  # if mouse moved
+		angle = event.relative.x * aim_speed
+		canon.rotate_y(-angle)
 	elif event.is_action_pressed("action"):  # if spacebar is pressed
 		fire()  # shoot the canonball
 
@@ -36,4 +44,11 @@ func fire() -> void:
 	loaded = false
 
 func on_exit() -> void:
+	canon.rotation = Vector3(0,0,0)
+	player.rotation = Vector3(0,0,0)
+	player.position = Vector3(0,0,0)
 	player.reparent(world)
+	player.global_position = canon.global_position + Vector3(-2,0,0)
+	canon.rotation_degrees = Vector3(0,0,-80)
+	#player.forward_direction = 0.0
+	#camera.setup_camera()
