360a6fa437a48837be32a0cd1d53bd5c51d34898
Jennifer Danielson <apdan150@gmail.com>
2025-05-06 18:39:44 -0700

    Added doors.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -4,7 +4,7 @@
 extends Node
 
 var total_items = []  # all available items
-var current_item = ""  # can only have 1 item equipped at a time
+var current_item = "Circus Key"  # can only have 1 item equipped at a time
 
 
 func _ready() -> void:  # when the node is ready
--- Scripts/Objects/Interactables/circus_door.gd
+++ Scripts/Objects/Interactables/circus_door.gd
@@ -7,9 +7,9 @@ extends Interactable
 @export var Pivot2 : Node3D
 
 
-func use(player: Player) -> void:
-	if GameManager.collected_items.has("Circus Key"):
-		GameManager.collected_items.erase("Circus Key")
+func use (player: Player) -> void:
+	if GameManager.current_item == "Circus Key":
+		GameManager.current_item = ""
 		open_door()
 		
 		# TASK: Add an animation showing the door opening
@@ -22,5 +22,5 @@ func use(player: Player) -> void:
 func open_door():
 	var tween1 = create_tween()
 	var tween2 = create_tween()
-	tween1.tween_property(Pivot1, "rotation", Vector3(0, -PI/2, 0), 1)
-	tween2.tween_property(Pivot2, "rotation", Vector3(0, PI/2,0), 1)
+	tween1.tween_property(Pivot1, "rotation", Vector3(0, PI/2, 0), 1)
+	tween2.tween_property(Pivot2, "rotation", Vector3(0, -PI/2,0), 1)
