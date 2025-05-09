f4f07e1bd0ebbed545b5a663ad39f42ab19bc559
CohenK <cohenkang99@hotmail.com>
2025-05-08 19:39:19 -0700

    finished implementing canon

--- Scripts/Objects/Interactables/canon.gd
+++ Scripts/Objects/Interactables/canon.gd
@@ -3,13 +3,21 @@
 extends Interactable
 @export var canon: Node3D
 @export var ray: RayCast3D
+var loaded = true
 
 func use(player: Player) -> void:
 	var canon_state = player.statemachine.get_node("Canon State")
 	canon_state.canon = canon
-	player.reparent(canon)
-	player.statemachine.change_state("Canon State")
-
+	if loaded:
+		player.reparent(canon)
+		player.statemachine.change_state("Canon State")
+		player.position = Vector3(0, 1, 0)
+		canon.ray.visible = true
+	else:
+		if GameManager.current_item == "Canonball":
+			GameManager.current_item = ""
+			loaded = true
+	
 func fire() -> void:
 	var explosion = canon.get_node("Explosion")
 	explosion.explode()
--- Scripts/Player/State Machine/canon_state.gd
+++ Scripts/Player/State Machine/canon_state.gd
@@ -2,14 +2,12 @@
 
 extends StateMachineState
 
-
 @export var player : Player
 @export var canon: Node3D
 @export var camera: Camera3D
 @export var angle: float
 
 var world : Node3D = null
-var loaded = true
 var aim_speed : float = 0.005
 
 func on_ready() -> void:
@@ -17,15 +15,9 @@ func on_ready() -> void:
 	
 func on_enter() -> void:
 	player.velocity = Vector3.ZERO
-	if loaded:
-		player.position = Vector3(-0.5, -0.5, 0) # up/down, forward/backward, left/right
-		canon.ray.visible = true
-	else:
-		if GameManager.current_item == "Canonball":
-			GameManager.current_item = ""
-			loaded = true
-		else:
-			change_state("Exploration State")
+	camera.look_at_position(Vector3(canon.global_position.x+100,canon.global_position.y,canon.global_position.z))
+	player.position = Vector3(0, 1, 0)
+	canon.ray.visible = true
 
 func on_process(_delta: float) -> void:
 	player.update_input()  # take the player's input
@@ -37,15 +29,14 @@ func on_input(event: InputEvent) -> void:
 		angle = event.relative.x * aim_speed
 		canon.rotate_y(-angle)
 	elif event.is_action_pressed("action"):  # if spacebar is pressed
-		if loaded == true:
+		if canon.loaded:
 			canon.fire()  # shoot the canonball
-			loaded = false
-
+			canon.loaded = false
 
 func on_exit() -> void:
-	canon.rotation_degrees = Vector3(0, 0, -90)
+	canon.rotation_degrees = Vector3(0, 0, 0)
 	player.reparent(world)
 	player.global_position = canon.global_position + Vector3(-1.5, 0, 0)
-	player.rotation_degrees = Vector3(0,0,90)
+	player.rotation_degrees = Vector3(0,0,0)
 	canon.ray.visible = false
 	
