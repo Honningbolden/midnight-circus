b01f2c45af57e4039337cd6d9d5196070d395611
Vin <justvindictivity@gmail.com>
2025-05-08 23:59:13 -0700

    Revert "added sounds to olaf, updated SteamEngine script, and fixed issue with vending machine"
    This reverts commit dba7a4679851ef60936268874a4a5664219b6a71.

--- Scripts/Objects/Interactables/steam_engine.gd
+++ Scripts/Objects/Interactables/steam_engine.gd
@@ -2,16 +2,13 @@
 
 extends Interactable
 @export var valve: MeshInstance3D
-@export var speaker: AudioStreamPlayer3D
+
 
 func use(_player: Player) -> void:
+	$IdleSound.play()
 	valve.visible = true
 	var tween = create_tween()
 	tween.tween_property(valve, "rotation_degrees", Vector3(90,0,0), 1)
 	if GameManager.current_item == "Valve":
 		GameManager.current_item = ""
-		$IdleSound.play()
-		valve.visible = true
-		var tween = create_tween()
-		tween.tween_property(valve, "rotation_degrees", Vector3(0,90,0), 1)
-		speaker.play()
+		
