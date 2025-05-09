dba7a4679851ef60936268874a4a5664219b6a71
D1MES <d.melgar0905@gmail.com>
2025-05-08 23:30:13 -0700

    added sounds to olaf, updated SteamEngine script, and fixed issue with vending machine

--- Scripts/Objects/Interactables/olaf.gd
+++ Scripts/Objects/Interactables/olaf.gd
@@ -5,16 +5,24 @@ extends Interactable
 
 @export var key : Node3D
 
+func _ready():
+	randomize()
+
 func use(_player: Player) -> void:
 	if GameManager.current_item == "Vodka":
 		GameManager.current_item = ""
+		$Snore.stop()
 		await wait(1) #waits 1 second before giving you item
 		key.position = self.global_position
 		key.position.x += 1
 		# TASK: Add an animation Giving the Vodka to Olaf
-		# TASK: Add a sound effect for Olaf gving the key
+		$Keys.play()
 	else:
-		pass
+		var mumbles = [$Mumble1, $Mumble2, $Mumble3]
+		var idx = randi() % mumbles.size()
+		for p in mumbles:
+			p.stop()
+		mumbles[idx].play()
 		# TASK: Add a sound effect for when the player doesn't have vodka
 
 func wait(seconds: float) -> void:        # Thanks to GBWD on the Godot forum
