728aeed3b0a9a6614c1b1a7cfe359b6bbb0a2309
Vin <justvindictivity@gmail.com>
2025-05-09 16:31:56 -0700

    hide issues under rug

--- Scripts/Objects/Interactables/rope.gd
+++ Scripts/Objects/Interactables/rope.gd
@@ -9,6 +9,6 @@ func use(_player: Player) -> void:
 		usable = false
 		var tween = create_tween()
 		var x = cannonball.global_position.x
-		var y = 0.5
+		var y = 0.7
 		var z = cannonball.global_position.z
 		tween.tween_property(cannonball, "global_position", Vector3(x, y, z), 2)
