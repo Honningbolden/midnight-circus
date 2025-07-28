bddb2eb4b3010fb7261afd9ae5e6bfcc0769b8e9
Angelo Cervana <152583644+angeloose@users.noreply.github.com>
2025-07-28 12:11:24 -0700

    Removing cohen print statement

--- Scripts/Objects/Interactables/canon_ray.gd
+++ Scripts/Objects/Interactables/canon_ray.gd
@@ -3,4 +3,4 @@ extends RayCast3D
 func _process(delta: float) -> void:
 	if is_colliding():
 		var collider = self.get_collider()
-		print("colliding with:", collider)
+		#print("colliding with:", collider)
